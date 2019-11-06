952718a8ca52048120c0e761db5bfeb15caaec3c
RMH <rory.hennessey@gmail.com>
2019-11-05 19:17:44 -0500

    contact form created

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,55 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+    ADD_CONTACT,
+    DELETE_CONTACT,
+    SET_CURRENT,
+    CLEAR_CURRENT,
+    UPDATE_CONTACT,
+    FILTER_CONTACTS,
+    CLEAR_FILTER
+} from '../types'
+
+const ContactState = props => {
+    const initialState = {
+        contacts: []
+    }
+
+    const [state, dispatch] = useReducer(contactReducer, initialState);
+
+    // Add Contact
+    const addContact = contact => {
+        contact.id = uuid.v4();
+        dispatch({ type: ADD_CONTACT, payload: contact });
+    }
+
+    // Delete Contact
+    const deleteContact = id => {
+        dispatch({ type: DELETE_CONTACT, payload: id });
+    }
+
+    // Set Current Contact
+
+    // Clear Current Contact
+
+    // Update Contact
+
+    // Filter Contacts
+
+    // Clear Filter
+
+    return (
+        <ContactContext.Provider
+            value={{
+                contacts: state.contacts,
+                addContact,
+                deleteContact
+            }}>
+            {props.children}
+        </ContactContext.Provider>
+    )
+}
+
+export default ContactState;
